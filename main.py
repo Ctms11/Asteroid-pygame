34da5bc7074ff5b6eb1b01bdb0b37caae14a9b68
Ctms11 <clgraves2000@gmail.com>
2026-02-05 23:50:17 -0600

    D: Changes made

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     VERSION = pygame.version.ver
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version:{VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+   
 
     while True:
         log_state()
@@ -18,9 +20,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.draw(screen)    
         pygame.display.flip()
         dt = pygame.time.Clock().tick(60) / 1000.0
-        print(dt)
+        
+        
     
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+import pygame
+from constants import PLAYER_RADIUS, LINE_WIDTH
 class Player(CircleShape):
     rotation = 0
     def __init__(self, x, y):
@@ -16,3 +17,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
